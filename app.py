3c08f0d1ae73fff5724f12e0aaf9f1086b6b2ab4
Amasha1009 <ITBIN-2313-0032@horizoncampus.edu.lk>
2026-08-15 17:29:09 +0530

    Fix Streamlit prediction workflow

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-st.title("Wildlife Footprint & Animal Identification")
+st.title("🐾 Wildlife Footprint & Animal Identification")
 
 class_names = [
-    "bear",
-    "deer",
-    "elephant",
-    "leopard",
-    "wild_boar",
-    "wolf"
+    "🐻bear",
+    "🦌deer",
+    "🐘elephant",
+    "🐆leopard",
+    "🐗wild_boar",
+    "🐺wolf"
 ]
 
-st.sidebar.header("About the Project")
+# Sidebar
+st.sidebar.header("ℹ️ About the Project")
 
 st.sidebar.write(
     "This application identifies wildlife animals "
@@ -23,19 +24,18 @@ st.sidebar.write(
 )
 
 st.sidebar.write("**Model:** MobileNetV3Small")
-
-st.sidebar.write("**Test Accuracy:** 100%")
+st.sidebar.write("**Test Accuracy:** 70%")
 
 st.sidebar.subheader("Supported Animals")
 
 for animal in class_names:
-    st.sidebar.write(f"• {animal}")
+    st.sidebar.write(f"- {animal}")
 
+# Load model
 MODEL_PATH = "model/best_wildlife_model.keras"
-
 model = tf.keras.models.load_model(MODEL_PATH)
 
-
+# Upload image
 uploaded_file = st.file_uploader(
     "Upload a wildlife image",
     type=["jpg", "jpeg", "png"]
@@ -47,36 +47,47 @@ if uploaded_file is not None:
 
     st.image(
         image,
-        caption="Uploaded Image"
+        caption="📁Uploaded Image"
     )
 
+    # Button
     if st.button("Identify Animal"):
 
-        image = image.resize((224, 224))
+        # Resize image
+        resized_image = image.resize((224, 224))
 
-    image_array = np.array(image)
+        # Convert image to NumPy array
+        image_array = np.array(resized_image)
 
-    image_array = np.expand_dims(image_array, axis=0)
+        # Add batch dimension
+        image_array = np.expand_dims(
+            image_array,
+            axis=0
+        )
 
-    predictions = model.predict(
-        image_array,
-        verbose=0
-    )
+        # Make prediction
+        predictions = model.predict(
+            image_array,
+            verbose=0
+        )
 
-    predicted_index = np.argmax(predictions[0])
+        # Get predicted class
+        predicted_index = np.argmax(predictions[0])
 
-    predicted_animal = class_names[predicted_index]
+        predicted_animal = class_names[predicted_index]
 
-    confidence = float(
-        predictions[0][predicted_index]
-    ) * 100
+        # Get confidence
+        confidence = float(
+            predictions[0][predicted_index]
+        ) * 100
 
-    st.subheader("Prediction")
+        # Display result
+        st.subheader("🎯Prediction")
 
-    st.write(
-        f"Animal: **{predicted_animal}**"
-    )
+        st.write(
+            f"🐾Animal: **{predicted_animal.replace('_', ' ').title()}**"
+        )
 
-    st.write(
-        f"Confidence: **{confidence:.2f}%**"
-    )
+        st.write(
+            f"📊Confidence: **{confidence:.2f}%**"
+        )
